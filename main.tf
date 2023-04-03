cbe1f0e2da22bb450d97c8a3f3e1c99142fb70ae
AbbyKuo <114968245+AbbyKuo@users.noreply.github.com>
2023-04-03 11:08:04 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@
 # }
 provider "aws" {
   region                  = "ap-southeast-2"
-#   profile                 = "PNTerraform"
-#   shared_credentials_file = "~/.aws/credentials"
+  profile                 = "PNTerraform"
+  shared_credentials_file = "~/.aws/credentials"
 }
 # provider "aws" {
 #   alias  = "cloudfront-acm-certs"
